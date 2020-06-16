bc85ba0cfc8b59bbb85713a90cf8f64dbb8e17f4
bren2102 <brb.yg.21@gmail.com>
2020-06-16 12:18:26 -0500

    Create api connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './assets/styles/styles.scss';
+
+const apiRequest = 'http://api.openweathermap.org/data/2.5/weather?appid=a6173c3db3d0ef08cdae03efbc18c2c8';
+
+const weatherResult = (city) => {  
+  const citySearch = apiRequest + '&q=lima';
+  return new Promise((resolve, reject) => {
+    fetch(citySearch)
+    .then(response.json().then((json) => {
+      const weather = json.wind.speed;
+      resolve(weather);
+      console.log(weather);
+    }));
+  });
+};
